f1993f5670801ac40ccbc1339f168471794cccb2
Keanan Koppenhaver <keanan@doejo.com>
2016-05-22 21:34:20 -0500

    comments

--- jquery.filters.js
+++ jquery.filters.js
@@ -1,17 +1,22 @@
 (function($) {
 
     $.fn.filters = function() {
+    	// Grab whatever container the plugin was init'ed on
     	var $filters_container = $(this);
       
+      // Loop through each filter in the container
+      // Make sure we return the each so that methods can be chained
       return this.children().each(function(){
       	
       	var $filter = $(this);
 
+      	// Adding a click event on each filter
       	$filter.click(function(){
+      		// Get rid of any existing active classes
       		$filters_container.children().removeClass('active');
 
+      		// Add an active class to the filter that was clicked
       		$filter.addClass('active');
-
       	});
 
       });
